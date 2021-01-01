a324641765907d22b63ac615f966c1cf02c4b269
hyun gyu <gusrb3164@gmail.com>
2021-01-01 17:53:38 +0900

    temp 2021-01-01

--- src/components/UI/ProblemInfo.js
+++ src/components/UI/ProblemInfo.js
@@ -1,31 +1,18 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import {
-  Grid,
-  Button,
-  Select,
-  MenuItem,
-  InputLabel,
-  FormControl,
-  TextField,
-} from '@material-ui/core';
-const useStyles = makeStyles((theme) => ({
-  infoBox: {
-    padding: '0% 1%',
-  },
-}));
-const ProblemInfo = ({ props }) => {
-  const classes = useStyles();
-  return (
-    <Grid container className={classes.infoBox} direction="column" xs={6}>
-      <h1>{props.title}</h1>
-      {props.problemInfo}
-      <h2>입력 형식</h2>
-      {props.inputType}
-      <h2>출력 형식</h2>
-      {props.outputType}
-    </Grid>
-  );
+import { Grid } from '@material-ui/core';
+
+const ProblemInfo = ({ title, problemInfo, inputType, outputType }) => {
+	return (
+		<Grid>
+			<h1>{title}</h1>
+			{problemInfo}
+			<h2>입력 형식</h2>
+			{inputType}
+			<h2>출력 형식</h2>
+			{outputType}
+		</Grid>
+	);
 };
 
 export default ProblemInfo;
